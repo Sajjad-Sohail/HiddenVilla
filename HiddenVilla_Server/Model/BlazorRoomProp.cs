d226e1febea52f1f788fc93ec4b23faeabf3d8fe
Sajjad <sajjadsohail124@gmail.com>
2021-06-23 19:25:48 -0400

    Add project files.

---
+++ HiddenVilla_Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoomProp
+    {
+        public string Name { set; get; }
+        public int ID { get; set; }
+        public  string Value { get; set; }
+
+    }
+}
