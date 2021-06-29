aae3b296096cdc386e626805e061c956b4abdc2e
Sajjad <sajjadsohail124@gmail.com>
2021-06-29 14:08:52 -0400

    Added Hotel Room list page along with add new room form

--- HiddenVilla_Server/Startup.cs
+++ HiddenVilla_Server/Startup.cs
@@ -1,3 +1,5 @@
+using BusinessLayer.Repository;
+using BusinessLayer.Repository.IRepository;
 using DataAccess.Data;
 using HiddenVilla_Server.Data;
 using Microsoft.AspNetCore.Builder;
@@ -30,7 +32,7 @@ namespace HiddenVilla_Server
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
+            services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddRazorPages();
             services.AddServerSideBlazor();
